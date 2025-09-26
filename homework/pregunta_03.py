26cae947e14750fb96f79dcfff919250409b1adb
LissyGrace <lissy.restrepo@gmail.com>
2025-09-26 04:27:00 -0500

    Lab 2 Finalizado

--- homework/pregunta_03.py
+++ homework/pregunta_03.py
@@ -15,3 +15,21 @@ def pregunta_03():
     [('A', 53), ('B', 36), ('C', 27), ('D', 31), ('E', 67)]
 
     """
+def pregunta_03():
+    """
+    Retorne la suma de la columna 2 por cada letra de la primera columna como
+    una lista de tuplas (letra, suma) ordendas alfabeticamente.
+
+    Rta/
+    [('A', 53), ('B', 36), ('C', 27), ('D', 31), ('E', 67)]
+
+    """
+    with open("files/input/data.csv", "r", encoding="utf-8") as file:
+        counts = {}
+        for line in file:
+            letter = line.split("\t")[0]
+            if letter in counts:
+                counts[letter] += int(line.split("\t")[1])
+            else:
+                counts[letter] = int(line.split("\t")[1])
+    return sorted(counts.items())
